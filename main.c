d8b49bcf62f34ded63fb71403e6544a4b1266c40
Erik Moe <edmoe@ntnu.stud.no>
2020-01-13 16:35:32 +0100

    test

--- main.c
+++ main.c
@@ -1,8 +1,3 @@
 int main(){
-        printf("Hello world\n");
-        printf("...and Mars\n");
-	if(1 < 0){
-        // In C, returning something non-zero signals an error return 1;
-	}
 	return 0; 
 }
